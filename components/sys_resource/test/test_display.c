6743f563f40961ee38fbd677c1aa945443087f50
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-10-28 19:31:33 -0400

    create unity rest framework

---
+++ components/sys_resource/test/test_display.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "unity.h"
+#include "sys_resource.h"
+
+TEST_CASE("TBD", "[args]")
+{
+
+    TEST_ASSERT_EQUAL(0, 0);
+}
--- test/main/main_test.c
+++ test/main/main_test.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "unity.h"
+
+void app_main() 
+{
+    UNITY_BEGIN();
+    unity_run_tests_by_tag("[double]", false);
+    UNITY_END();
+}
