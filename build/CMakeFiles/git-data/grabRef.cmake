48b588e883f86ca71e5ad8f51f790a34c3f3d0b1
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-10-28 19:10:02 -0400

    init components

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/Dmitr/OneDrive/Desktop/esp-idf/projects/Weather_Clock/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/Dmitr/OneDrive/Desktop/esp-idf/.git")
+set(GIT_DIR "C:/Users/Dmitr/OneDrive/Desktop/esp-idf/projects/Weather_Clock/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
