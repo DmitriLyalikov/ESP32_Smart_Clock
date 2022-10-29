8ddc381ee94838d38b895bd47bd8e860413fee83
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-10-29 00:23:42 -0400

    wifi event_handler and init station functions
    ok status

--- main/Weather_Clock.c
+++ main/Weather_Clock.c
@@ -24,7 +24,14 @@
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
     printf("********ESP32 Weather Clock Application********\n");
-
+    wifi_init_sta();
+    
 }
