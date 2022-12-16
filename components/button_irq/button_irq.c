f11efb2f0b4f2e6c002990e6e3eb568b401ea93d
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-12-16 15:46:46 -0500

    created isr definitions

--- components/button_irq/button_irq.c
+++ components/button_irq/button_irq.c
@@ -1,2 +1,19 @@
 #include "button_irq.h"
 
+static volatile uint8_t lcd_duty_cycle;
+
+
+void IRAM_ATTR gpio_brightness_inc_isr_handler(void* arg){
+
+    if (lcd_duty_cycle < 100) {
+        lcd_duty_cycle += 10;
+    }
+}
+
+void IRAM_ATTR gpio_brightness_dec_isr_handler(void* arg){
+
+    if (lcd_duty_cycle > 0) {
+        lcd_duty_cycle -= 10;
+    }
+}
+
