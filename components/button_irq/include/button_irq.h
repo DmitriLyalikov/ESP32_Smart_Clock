8e9dd795ae0e32b85ffa5a8a334a3eafce27233c
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-12-16 14:41:48 -0500

    added button_irq component
    defines button IRQ functions and init function to register them at runtime

---
+++ components/button_irq/include/button_irq.h
@@ -0,0 +1,35 @@
+/**
+ * @file button_irq.h
+ * @author Dmitri Lyalikov (Dlyalikov01@manhattan.edu)
+ * @brief  Weather Request functions between HTTP request and Display Queue
+ * @version 0.1
+ * @date 2022-11-14
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+#ifndef BUTTON_IRQ_H
+#define BUTTON_IRQ_H
+
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_intr_alloc.h"
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // BUTTON_IRQ_H
+
---
+++ components/button_irq/test/test_button_irq.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "unity.h"
+
+
+TEST_CASE("TBD", "[args]")
+{
+
+    TEST_ASSERT_EQUAL(0, 0);
+}
